f7219e7750284c4f722042af82db2b0864de37a7
Eric Katz <ericpkatz@gmail.com>
2015-07-30 01:21:02 +0000

    set up server side endpoints

--- public/app/app.js
+++ public/app/app.js
@@ -3,17 +3,47 @@ var app = angular.module("myWorld", ['ngRoute']);
 app.config(function($routeProvider, $locationProvider){
    $routeProvider
         .when("/", {
-           templateUrl: "/templates/home.html" 
+           templateUrl: "/templates/home.html",
+           controller: "HomeCtrl"
         })
         .when("/things", {
-            templateUrl: "/templates/things.html"
+            templateUrl: "/templates/things.html",
+            controller: "ThingsCtrl"
         })
         .when("/people", {
-            templateUrl: "/templates/people.html"
+            templateUrl: "/templates/people.html",
+            controller: "PeopleCtrl"
         });
+        
+        $locationProvider.html5Mode(true);
 });
 
 app.controller("MyCtrl", function($scope){
     console.log($scope);
     $scope.name = "professor";
+});
+
+var counter = 0;
+app.controller("HomeCtrl", function($scope){
+    console.log("hello");
+    $scope.today = new Date();
+    $scope.counter = ++counter;
+    
+});
+app.controller("ThingsCtrl", function($scope, $http){
+   $http.get("/api/things") 
+        .then(function(results){
+            $scope.things = results.data;
+        });
+    
+    $scope.remove = function(thing){
+        var index = $scope.things.indexOf(thing);
+        $scope.things.splice(index, 1);
+    };
+});
+app.controller("PeopleCtrl", function($scope, $http){
+    $http.get("/api/people")
+        .then(function(results){
+           $scope.people = results.data;
+        });
 });
--- server.js
+++ server.js
@@ -1,4 +1,27 @@
 var express = require("express");
+var mongoose = require("mongoose");
+var ThingSchema = mongoose.Schema({
+    name: String
+});
+
+var Thing = mongoose.model("thing", ThingSchema);
+
+var PersonSchema = mongoose.Schema({
+    name: String,
+    active: { type: Boolean, default: true }
+});
+
+var Person = mongoose.model("person", PersonSchema);
+
+mongoose.connect("mongodb://localhost:27017/my_world");
+
+mongoose.connection.on("open", function(){
+    console.log("connected");
+});
+
+mongoose.connection.on("error", function(err){
+    console.log(err);
+});
 
 var app = express();
 app.set("view engine", "jade");
@@ -7,6 +30,26 @@ app.use(express.static(__dirname + "/public"));
 
 app.get("/", function(req, res){
     res.render("index");
+});
+app.get("/things", function(req, res){
+    res.render("index");
+});
+app.get("/people", function(req, res){
+    res.render("index");
+});
+
+app.get("/api/things", function(req, res){
+   Thing.find({})
+    .then(function(things){
+        res.send(things); 
+    });
+});
+
+app.get("/api/people", function(req, res){
+   Person.find({})
+    .then(function(people){
+        res.send(people);
+    });
 });
 
 app.listen(process.env.PORT);
